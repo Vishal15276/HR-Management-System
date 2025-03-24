b51c2cf9ac707e237c316232cf26725fd0be1495
Vishal15276 <vishal15276@gmail.com>
2025-03-24 11:11:47 +0530

    react

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="bg-white h-screen flex items-center justify-center">
+      <h1 className="text-black text-3xl font-bold">HR Management System</h1>
+    </div>
+  );
+}
+
+export default App;
